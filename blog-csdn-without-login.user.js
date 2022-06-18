f25fac6bc80fa9c825d263303c4b6faa4c2edf4b
lebr0nli <alan.li.tw@gmail.com>
2022-06-18 14:44:26 +0800

    use setTimeout instead of document-idle

--- blog-csdn-without-login.user.js
+++ blog-csdn-without-login.user.js
@@ -2,8 +2,7 @@
 // @name        blog.csdn.net without login
 // @description Bypass the annoying login popup of blog.csdn.net and select/copy code without any problem.
 // @match       *://blog.csdn.net/*/article/*
-// @version     2022.06.18
-// @run-at      document-idle
+// @version     2022.06.18.1
 // @updateURL   https://raw.githubusercontent.com/lebr0nli/blog-csdn-without-login/main/blog-csdn-without-login.user.js
 // ==/UserScript==
 
@@ -16,33 +15,36 @@ function copyToClipboard(text) {
     textArea.remove();
 }
 
-document.body.querySelectorAll('.hljs-button.signin').forEach(node => {
-    node.dataset.title = 'Copy';
-    node.removeAttribute('data-report-click');
-    // Make mdcp.signin do nothing
-    node.setAttribute('onclick', 'window.mdcp ? window.mdcp.signin=(e)=>{} : null;');
-    // Copy the code
-    node.addEventListener('click', e => {
-        e.preventDefault();
-        copyToClipboard(node.parentElement.innerText);
-        node.dataset.title = 'Copied!';
-        setTimeout(() => {
-            node.dataset.title = 'Copy';
-        }, 1000);
+// wait for the page to load
+setTimeout(() => {
+    document.body.querySelectorAll('.hljs-button.signin').forEach(node => {
+        node.dataset.title = 'Copy';
+        node.removeAttribute('data-report-click');
+        // Make mdcp.signin do nothing
+        node.setAttribute('onclick', 'window.mdcp ? window.mdcp.signin=(e)=>{} : null;');
+        // Copy the code
+        node.addEventListener('click', e => {
+            e.preventDefault();
+            copyToClipboard(node.parentElement.innerText);
+            node.dataset.title = 'Copied!';
+            setTimeout(() => {
+                node.dataset.title = 'Copy';
+            }, 1000);
+        });
     });
-});
 
-document.body.querySelectorAll('code').forEach(node => {
-    // Chrome and Firefox
-    node.style.userSelect = 'text';
-    // Safari
-    node.style['-webkit-user-select'] = 'text';
-});
+    document.body.querySelectorAll('code').forEach(node => {
+        // Chrome and Firefox
+        node.style.userSelect = 'text';
+        // Safari
+        node.style['-webkit-user-select'] = 'text';
+    });
 
-document.addEventListener('copy', e => {
-    // avoid copyright text
-    if (e.clipboardData.getData('text/plain').includes('\r\n————————————————\r\n版权声明')) {
-        e.preventDefault();
-        e.clipboardData.setData('text/plain', document.getSelection().toString());
-    }
-});
+    document.addEventListener('copy', e => {
+        // avoid copyright text
+        if (e.clipboardData.getData('text/plain').includes('\r\n————————————————\r\n版权声明')) {
+            e.preventDefault();
+            e.clipboardData.setData('text/plain', document.getSelection().toString());
+        }
+    });
+}, 1000);
